044866ab6f6146ecf3cf0f94d69220f73baec2c7
Oly <hoit@bk.ru>
2016-10-14 18:36:40 +0300

    Update examples_with_boost.cpp

--- examples_with_boost.cpp
+++ examples_with_boost.cpp
@@ -7,11 +7,16 @@
 int main()
 {
 	std::string line;
-	boost::regex pat("^Subject: (Re: |Aw: )*(.*)");while (std::cin)
+	
+	boost::regex pat("^Subject: (Re: |Aw: )*(.*)");
+	
+	while (std::cin)
 	{
 		std::getline(std::cin, line);
 		boost::smatch matches;
 		if (boost::regex_match(line, matches, pat))
 			std::cout « matches[2] « std::endl;
-	}return 0;
+	}
+	
+	return 0;
 }
